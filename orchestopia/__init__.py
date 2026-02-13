6bf483531fdc4754a3c6f6f6813c9d3a56e160de
yysun0116 <jessie.yy.sun@gmail.com>
2026-02-13 20:55:35 +0800

    [docs] remove deprecated documentation

--- orchestopia/__init__.py
+++
@@ -1,7 +0,0 @@
-from builder.model import build_models
-from builder.format import build_formats
-from builder.mcp_tool import build_mcp_tools
-from builder.agent import build_agents
-
-
-__all__ = ["build_models", "build_formats", "build_mcp_tools", "build_agents"]
